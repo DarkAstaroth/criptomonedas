b03a7c8181f3e109db7689596a9bad15686c07df
Manuel <mninahuanca4@gmail.com>
2020-12-24 17:48:29 -0400

    agregando un segundo hook para las criptonomedas y creando el select con datos desde una API

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect , useState  } from 'react';
 import styled from '@emotion/styled';
 import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomonedas';
+import axios from 'axios';
 
 const Boton = styled.input`
     margin-top : 20px;
@@ -22,6 +24,10 @@ const Boton = styled.input`
 
 const Formulario = () => {
 
+    // state del listado de criptomonedas
+
+    const [listacripto, setListacripto] = useState([]);
+
     const MONEDAS = [
         { codigo: 'USD', nombre: 'Dolar de estados unidos' },
         { codigo: 'MXN', nombre: 'Peso Mexicano' },
@@ -31,12 +37,28 @@ const Formulario = () => {
     ]
     
     // Utilizar Moneda
+    const [moneda, SelectMonedas] = useMoneda('Elige tu moneda', '', MONEDAS);
+    
+    // Utilizar Criptomonedas
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda', '' , listacripto);
+    
+    //Ejecutar llamado a la API
+    useEffect(() => {
 
-    const [moneda, SelectMonedas] = useMoneda('Elige tu moneda','',MONEDAS);
+        const consultarAPI = async () => { 
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+            const resultado = await axios.get(url);
+            setListacripto(resultado.data.Data);
+        }
+
+        consultarAPI();
+    
+    }, []);
 
     return ( 
         <form action="">
-            <SelectMonedas/>
+            <SelectMonedas />
+            <SelectCripto/>
             <Boton
                 type="submit"
                 value="Calcular"
